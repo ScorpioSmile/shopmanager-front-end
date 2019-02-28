08f5841d7ed9110faed05b0c8e0d8291724de3a0
ScorpioYu <834952121@qq.com>
2019-02-28 08:50:49 +0800

    首页

---
+++ controllers/home.js
@@ -0,0 +1,29 @@
+//首页相关的业务路由
+const settingsModel = require('../models/settings')
+const productModel = require('../models/product')
+
+exports.index = (req, res, next) => {
+  //1. 渲染首页  轮播图数据
+  //2. 渲染首页  猜你喜欢数据
+  //Promise.race()  执行多个Promise对象  等最快的响应成功就会 调用成功的回调
+  //参数是  Promise 数组
+  Promise.all([settingsModel.getSliders(), productModel.getLikeProducts()])
+    .then(results => {
+      //results 是promise对象的返回值的集合 而且按照你传入的顺序
+      res.locals.sliders = results[0]
+      res.locals.likes = results[1]
+      res.render('home')
+    }).catch(err => next(err))
+}
+
+exports.like = (req, res, next) => {
+  //返回json格式的数据  猜你喜欢
+  productModel.getLikeProducts().then(data => {
+    res.json({code: 200, data: data})
+  }).catch(err => {
+    res.json({code: 500, msg: err.message})
+  })
+  //自己约定好的错误信息  200 成功  500 失败
+  //如果有错误不能交给统一错误中间件
+  //错误处理中间件 响应的是页面
+}
--- routers.js
+++ routers.js
@@ -1,1 +1,12 @@
 //集合所有的路由
+const express = require('express')
+const router = express.Router()
+const homeController = require('./controllers/home')
+
+//渲染首页
+router.get('/', homeController.index)
+//返回猜你喜欢的json数据
+router.get('/like', homeController.like)
+//todo 配置网站所有的路由
+
+module.exports = router
